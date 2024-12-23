b38409524d4fd0e148e104d878f0f076248a7733
niefba <fabien.tillon@rupil.fr>
2024-12-23 09:28:06 +0100

    Dark mode for input type date with color-scheme

--- client/src/app/ui/field-label.tsx
+++ client/src/app/ui/field-label.tsx
@@ -29,7 +29,7 @@ export function Input({id, init="", type="text", label, placeholder=label, disab
         <div className="flex flex-col space-y-1">
           <label htmlFor={id} className="text-lg font-medium text-gray-700 dark:text-gray-200">{label}</label>
           <input type={type} id={id} name={id} placeholder={placeholder} disabled={disabled} value={value} onChange={e => { setValue(e.target.value)}}
-            className="px-2 py-2 border border-zinc-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:border-transparent dark:bg-stone-700 dark:border-zinc-600"
+            className="px-2 py-2 border border-zinc-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:border-transparent dark:bg-stone-700 dark:border-zinc-600 dark:[color-scheme:dark]"
           />
         </div>
         {helper &&
